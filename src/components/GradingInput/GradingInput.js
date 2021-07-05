3d24596979bb2872738e66f495bbf4869ba7782a
FanFan <eric93504@gmail.com>
2021-07-05 20:57:53 +0800

    GradingInputs改變判定空分數的方式並新增參數

--- src/components/GradingInput/GradingInput.js
+++ src/components/GradingInput/GradingInput.js
@@ -3,20 +3,22 @@ import { Tr, Td } from "@chakra-ui/table";
 import { Input } from "@chakra-ui/input";
 export default function GradingInput(props) {
   const Alphabet = "ABCDE"; //印出ＡＢＣＤＥ組
+  const readOnly = props.readOnly ? props.readOnly : false;
   const scores = props.scores;
-  const rowIndex = props.rowIndex;
-  const onBlur = props.onBlurFunc;
+  const rowIndex = props.rowIndex?props.rowIndex:null;
+  const onBlur = props.onBlurFunc ? props.onBlurFunc : () => {};
   return (
     <Tr key={rowIndex}>
       {scores.map((score, index) => {
-        if (score === null || score._id === "000000000000000000000000") {
+        if (score === null || score.isEmpty === true) {
           return (
             <Td key={index}>
               <Input
+                isReadOnly={readOnly}
                 onBlur={(e) => {
                   onBlur(e, index + 1, rowIndex + 1);
                 }}
-                placeholder={`輸入${rowIndex + 1}${Alphabet[index]}分數`}
+                placeholder={ rowIndex ? `輸入${rowIndex + 1}${Alphabet[index]}分數` : '空白' }
               />
             </Td>
           );
@@ -24,6 +26,7 @@ export default function GradingInput(props) {
           return (
             <Td key={index}>
               <Input
+                isReadOnly={readOnly}
                 onBlur={(e) => {
                   onBlur(e, score.row, rowIndex + 1);
                 }}
