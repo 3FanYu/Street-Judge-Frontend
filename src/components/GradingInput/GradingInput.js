eba7608fb6289190e7cbb32af413b49b94c479c3
FanFan <eric93504@gmail.com>
2021-07-11 19:29:50 +0800

    complete settlePage logic

--- src/components/GradingInput/GradingInput.js
+++ src/components/GradingInput/GradingInput.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Tr, Td } from "@chakra-ui/table";
 import { Input } from "@chakra-ui/input";
+import { Tag } from "@chakra-ui/tag";
+import { Badge, InputGroup, InputLeftElement } from "@chakra-ui/react";
+
 export default function GradingInput(props) {
   const Alphabet = "ABCDE"; //印出ＡＢＣＤＥ組
   const readOnly = props.readOnly ? props.readOnly : false;
   const scores = props.scores;
-  const rowIndex = props.rowIndex?props.rowIndex:null;
+  const rowIndex = props.rowIndex ? props.rowIndex : null;
   const onBlur = props.onBlurFunc ? props.onBlurFunc : () => {};
+  const rankInput = props.rankInput ? props.rankInput : 0;
+  const isOT = props.isOT ? props.isOT : false;
+  const isFloat = (n) => {
+    return Number(n) === n && n % 1 !== 0;
+  };
   return (
     <Tr key={rowIndex}>
+      <Td>
+        <Tag variant="solid" colorScheme="teal">
+          {rowIndex + 1}
+        </Tag>
+      </Td>
       {scores.map((score, index) => {
         if (score === null || score.isEmpty === true) {
           return (
@@ -18,22 +31,30 @@ export default function GradingInput(props) {
                 onBlur={(e) => {
                   onBlur(e, index + 1, rowIndex + 1);
                 }}
-                placeholder={ rowIndex ? `輸入${rowIndex + 1}${Alphabet[index]}分數` : '空白' }
+                placeholder={
+                  readOnly
+                    ? "未輸入"
+                    : `輸入${rowIndex + 1}${Alphabet[index]}分數`
+                }
               />
             </Td>
           );
         } else {
           return (
             <Td key={index}>
+              <InputGroup>
+              {isOT && score.rank == rankInput ? <InputLeftElement pointerEvents="none" children={<Badge colorScheme="purple" >OT</Badge>} /> : <></>}
               <Input
+                bg={score.rank <= rankInput ? "teal" : ""}
                 isReadOnly={readOnly}
                 onBlur={(e) => {
                   onBlur(e, score.row, rowIndex + 1);
                 }}
                 placeholder={`輸入${score.number}${Alphabet[score.row]}分數`}
-                defaultValue={score.point}
+                defaultValue={Math.round(score.point*100)/100}
                 id={score._id}
-              />
+              ></Input>
+              </InputGroup>
             </Td>
           );
         }
--- src/pages/Settlement/Settlement.js
+++ src/pages/Settlement/Settlement.js
@@ -4,58 +4,176 @@ import { useParams } from "react-router";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import GradingInput from "../../components/GradingInput/GradingInput";
 import { Table, Thead, Tbody, Tr, Th } from "@chakra-ui/table";
+import { Center } from "@chakra-ui/layout";
+import { Tag } from "@chakra-ui/tag";
+import { Input } from "@chakra-ui/input";
 
 export default function Settlement() {
   const Alphabet = "ABCDE"; //印出ＡＢＣＤＥ組
   let { eventID } = useParams(); // 取得網址上的參數
-  const [AllScores, setAllScores] = useState([]);
-  const [AddUps, setAddUps] = useState([]);
-  const getTableHeader = (rowNum) => {
+  const [AllScores, setAllScores] = useState([]); // 所有評審的分數
+  const [AddUps, setAddUps] = useState([]); // 總成績
+  const [RankMap, setRankMap] = useState({}); // 紀錄 AllScores 的排名
+  const [AddUpsRankMap, setAddUpsRankMap] = useState({}); // 紀錄 總成績 的排名
+  const [DisplayBest, setDisplayBest] = useState(null); // 顯示用的名次篩選
+  const [RealBest, setRealBest] = useState(0); // 真正的名次（存在 RankMap AddUpsRankMap 的 名次）
+  const changeRankMap = (index) => { // tab切換時改變 RankMap
+    let tmpMap = {};
+    const scores = AllScores[index].scores;
+    const rowNum = AllScores[index].rowNum;
+    for (let i = 0; i < scores.length; i++) { // loop through 所有分數，並把名次存進RankMap
+      for (let j = 0; j < rowNum; j++) {
+        if (tmpMap[scores[i][j].rank] == undefined) {
+          tmpMap[scores[i][j].rank] = false;
+        }else{ // 若有重複的排名就設定true 代表 可能有ot的狀況
+          tmpMap[scores[i][j].rank] = true;
+        }
+      }
+    }
+    setRankMap(tmpMap);
+  };
+  const getTableHeader = (rowNum) => { //產生 TableHeader 
     let headerArray = [];
     for (let i = 0; i < rowNum; i++) {
-      const header = <Th key={i}>{Alphabet[i]}</Th>;
+      const header = (
+        <Th key={i}>
+          <Center>
+            <Tag colorScheme="teal">{Alphabet[i]}</Tag>
+          </Center>
+        </Th>
+      );
       headerArray.push(header);
     }
     return headerArray;
   };
+  const handleBestChange = (event, index) => { 
+    //當 "取幾強" 輸入匡的直遭到改動，除了改變DisplayRank(顯示用)，也背景運算出真實排名（RankMap裡真實存在的排名）
+    let value = event.target.value;
+    const rankmap = index ? RankMap : AddUpsRankMap;
+    if (value === null || value === undefined || value === "") { // 若輸入匡為空的則直接把值設為0
+      value = 0;
+    }
+    setDisplayBest(value); // 先設定顯示用的排名
+    while (rankmap[value] === undefined && value > 0) { 
+      // 計算真實排名，若輸入的排名不存在rankmap中就 -1 直到排名存在為止
+      value -= 1;
+    }
+    setRealBest(value); // 設定真實排名
+  };
+
   useEffect(async () => {
     try {
-      const res = await Api.get("settlement?eventID=" + eventID);
+      const res = await Api.get("settlement?eventID=" + eventID); // get api
       const data = res.data;
-      if (data.message === true) {
-        setAllScores(data.allScores);
-        setAddUps(data.addUps);
+      if (data.message === true) { // 成功取得資料就將資料存進hook中
+        const allScores = data.allScores;
+        const addUps = data.addUps;
+        setAllScores(allScores);
+        setAddUps(addUps);
+        let tmpMap = {};
+        for (let i = 0; i < addUps.length; i++) {
+          for (let j = 0; j < allScores[0].rowNum; j++) {
+            if (tmpMap[addUps[i][j].rank] == undefined) {
+              tmpMap[addUps[i][j].rank] = false;
+            }else{
+              tmpMap[addUps[i][j].rank] = true;
+            }
+          }
+        }
+        setAddUpsRankMap(tmpMap); // 設定 AddUpsRankMap
       }
     } catch (err) {
-      console.log(err);
+      alert(err)
     }
   }, []);
   return (
     <>
       <Tabs>
         <TabList>
+          <Tab>總成績</Tab>
           {AllScores ? (
-            AllScores.map((judge) => {
-              return <Tab>{judge.name}</Tab>;
+            AllScores.map((judge, index) => {
+              return (
+                <Tab
+                  onClick={() => {
+                    changeRankMap(index);
+                  }}
+                >
+                  {judge.name}
+                </Tab>
+              );
             })
           ) : (
             <></>
           )}
-          <Tab>總成績</Tab>
         </TabList>
 
         <TabPanels>
+          <TabPanel>
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Input
+                    colorScheme="whiteAlpha"
+                    size="lg"
+                    placeholder="輸入晉級名額數"
+                    value={DisplayBest}
+                    onChange={(v) => {
+                      handleBestChange(v);
+                    }}
+                  />
+                  {AddUps[0] ? getTableHeader(AddUps[0].length) : <></>}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {AddUps ? (
+                  AddUps.map((scores, index) => {
+                    return (
+                      <GradingInput
+                        scores={scores}
+                        readOnly={true}
+                        rowIndex={index}
+                        rankInput={RealBest}
+                        isOT={AddUpsRankMap[RealBest]}
+                      />
+                    );
+                  })
+                ) : (
+                  <></>
+                )}
+              </Tbody>
+            </Table>
+          </TabPanel>
           {AllScores ? (
-            AllScores.map((judge) => {
+            AllScores.map((judge, index) => {
               return (
                 <TabPanel>
                   <Table variant="striped">
                     <Thead>
-                      <Tr>{getTableHeader(judge.rowNum)}</Tr>
+                      <Tr>
+                        <Input
+                          colorScheme="whiteAlpha"
+                          size="lg"
+                          placeholder="輸入晉級名額數"
+                          value={DisplayBest}
+                          onChange={(v) => {
+                            handleBestChange(v, index);
+                          }}
+                        />
+                        {getTableHeader(judge.rowNum)}
+                      </Tr>
                     </Thead>
                     <Tbody>
-                      {judge.scores.map((scores) => {
-                        return <GradingInput scores={scores} readOnly={true} />;
+                      {judge.scores.map((scores, index) => {
+                        return (
+                          <GradingInput
+                            scores={scores}
+                            readOnly={true}
+                            rowIndex={index}
+                            rankInput={RealBest}
+                            isOT={RankMap[RealBest]}
+                          />
+                        );
                       })}
                     </Tbody>
                   </Table>
@@ -65,15 +183,6 @@ export default function Settlement() {
           ) : (
             <></>
           )}
-          <TabPanel>
-            {AddUps ? (
-              AddUps.map((scores) => {
-                return <GradingInput scores={scores} readOnly={true} />;
-              })
-            ) : (
-              <></>
-            )}
-          </TabPanel>
         </TabPanels>
       </Tabs>
     </>
