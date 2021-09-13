75f706ed071753ef6b56092d81f6e87ef09a3452
FanFan <eric93504@gmail.com>
2021-09-13 20:39:11 +0800

    change api route  to env var

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-require('dotenv').config()
 
 const { REACT_APP_API_URL } = process.env;
-const Api = axios.create({ baseURL: `${REACT_APP_API_URL}` });
+const Api = axios.create({ baseURL: REACT_APP_API_URL });
 export default Api;
